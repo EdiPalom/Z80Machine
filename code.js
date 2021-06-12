0f023aa9e77d6bd1bf7a09b3cd0ff30d8adae90f
Edisson Palomares <palomaredif@gmail.com>
2021-06-12 18:56:42 -0500

    added screen with canvas 2d

--- code.js
+++ code.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+
+window.onload = (()=>{
+    const canvas = document.querySelector("#canvas");
+
+    const context = canvas.getContext("2d");
+
+    const width = canvas.width;
+    const height = canvas.height;
+
+    const screenData = context.getImageData(0,0,width,height);
+
+    let data = screenData.data;
+
+    for(let i = 0; i < width * height; i++)
+    {
+        let index = i * 4;
+        data[index] = 0;
+        data[index + 1] = 0;
+        data[index + 2] = 170;
+        data[index + 3] = 255;
+    }
+
+    context.putImageData(screenData,0,0);
+})();
+
