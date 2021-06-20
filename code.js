24fdb6c533121e14a0c57c9e47808969e9b7c2eb
Edisson Palomares <palomaredif@gmail.com>
2021-06-20 18:39:44 -0500

    added set updates per second option in machine gui

--- code.js
+++ code.js
@@ -1,109 +1,84 @@
 'use strict';
-
 import {Machine} from './machine'
 import {MemGUI} from './memgui'
+import {MainLoop} from './mainloop';
+
+window.get_gaddress = ()=>{
+    let a = document.getElementById("go").value;
+    fill_memory(a);
+};
+
+window.set_pc = ()=>{
+    let a = document.getElementById("i_pc").value;
+    Machine.set_pc(a);
+};
+
+window.set_ups = ()=>{
+    let a = document.getElementById("i_ups").value;
+    MainLoop.set_ups(Number(a));
+};
+
+window.save = ()=>{
+    let elements = MemGUI.get_content();
+    let a = elements.split("\n\n");
+
+    for(let i = 0; i < 3; i++)
+    {
+        load_into_memory(a);
+    }
+};
+
+function load_into_memory(a,index){
+
+        let memory = a.splice(0,33);
+
+        let address = parseInt(memory[0],16);
+
+        memory.splice(0,1);
+
+        let bytes = [];
+
+        for(let i = 0; i < memory.length; i+=2)
+        {
+            let bt = memory[i] + memory[i+1];
+
+            bytes.push(parseInt(bt,16));
+        }
+
+        Machine.set_memory(address,bytes);
+};
+
+function show_memory(address)
+{
+    address = Number(address);
+    MemGUI.fill(Machine.get_memory(address),address);
+};
+
+function fill_memory(address)
+{
+    address = Number(address);
+    MemGUI.reset();
+    show_memory(address);
+    show_memory(address + 0x10);
+    show_memory(address + 0x20);
+};
+
+function start_machine()
+{
+    Machine.reset();
+    fill_memory(0x4000);
+};
+
+let App = {
+    start: ()=>{
+        start_machine();
+        MainLoop.loop();
+    },
+};
 
 window.onload = (()=>{
-
-    // let Machine = require('./machine');
-    //
-    // let mac = Machine("Hello");
-    //
-    // MemGUI.fill(Machine.get_memory(0x4000),0x4000);
-    start_machine();
-
-    window.get_gaddress = ()=>{
-        let a = document.getElementById("go").value;
-        fill_memory(a);
-    };
-
-    window.set_pc = ()=>{
-        let a = document.getElementById("i_pc").value;
-        Machine.set_pc(a);
-    };
-
-    window.save = ()=>{
-        let elements = MemGUI.get_content();
-        let a = elements.split("\n\n");
-
-        for(let i = 0; i < 3; i++)
-        {
-            load_into_memory(a);
-        }
-    };
-
-    function load_into_memory(a,index){
-
-            let memory = a.splice(0,33);
-
-            let address = parseInt(memory[0],16);
-
-            memory.splice(0,1);
-
-            let bytes = [];
-
-            for(let i = 0; i < memory.length; i+=2)
-            {
-                let bt = memory[i] + memory[i+1];
-
-                bytes.push(parseInt(bt,16));
-            }
-
-            Machine.set_memory(address,bytes);
-    }
-
-    function show_memory(address)
-    {
-        address = Number(address);
-        MemGUI.fill(Machine.get_memory(address),address);
-    };
-
-    function fill_memory(address)
-    {
-        address = Number(address);
-        MemGUI.reset();
-        show_memory(address);
-        show_memory(address + 0x10);
-        show_memory(address + 0x20);
-    }
-
-    function start_machine()
-    {
-        Machine.reset();
-        fill_memory(0x4000);
-        draw();
-    }
-
-    // window.run = ()=>{
-    //     Machine.read_program();
-    // };
-
-    function draw()
-    {
-        Machine.raster();
-        Machine.update_canvas();
-    };
-
-    let app = window.setInterval(()=>{
-            Machine.read_program();
-            draw();
-    },128);
-
-    window.run = ()=>{
-        window.stop();
-        app = setInterval(()=>{
-            Machine.read_program();
-            draw();
-        },128);
-    };
-
-    window.stop = ()=>{
-        clearInterval(app);
-    };
-
-    window.reset = ()=>{
-        stop();
-        start_machine();
-    };
+    App.start();
 })();
+
+
 
---
+++ mainloop.js
@@ -0,0 +1,78 @@
+import {Machine} from './machine'
+
+var MainLoop = function(){
+    let UPS = 30;
+    let MS_PER_SECOND = 1000 / UPS;
+
+    const getAnimation = (callback)=>{
+            let id = window.requestAnimationFrame(callback) || window.mozRequestAnimationFrame(callback) || window.msRequestAnimationFrame(callback) || window.webkitRequestAnimationFrame(callback);
+        return id;
+    };
+
+    let mainLoop = {
+        id:null,
+        last_time: Date.now(),
+        current_time: Date.now(),
+        ups_lag:0,
+        delta_time: 0,
+        previous_time: 0,
+        aps: 0,
+        fps:0,
+        quit:false,
+
+        loop:(time_elapsed)=>{
+
+            mainLoop.id = getAnimation(mainLoop.loop);
+
+            mainLoop.current_time = Date.now();
+            mainLoop.delta_time = mainLoop.current_time - mainLoop.last_time;
+            mainLoop.last_time = mainLoop.current_time;
+            mainLoop.ups_lag += mainLoop.delta_time;
+
+            if(mainLoop.ups_lag >= MS_PER_SECOND)
+            {
+                mainLoop.update();
+                mainLoop.ups_lag -= MS_PER_SECOND; 
+                // console.log(mainLoop.ups_lag);
+            }
+
+            mainLoop.draw();
+
+            if(time_elapsed - mainLoop.previous_time > 999){
+                // console.log(typeof(delta_time));
+                mainLoop.previous_time = time_elapsed; 
+
+                console.log("APS: " + mainLoop.aps + " FPS: " + mainLoop.fps);
+
+                mainLoop.aps = 0;
+                mainLoop.fps = 0;
+            }
+        },
+
+        stop:()=>{
+            window.cancelAnimationFrame(mainLoop.id);
+        },
+
+        update:(current_time)=>{
+            Machine.read_program();
+            mainLoop.aps++;
+        },
+
+        draw:(current_time)=>{
+            Machine.raster();
+            Machine.update_canvas();
+            mainLoop.fps++;
+        }
+    };
+
+    const set_ups = (ups)=>{
+        MS_PER_SECOND = 1000 / ups;
+    };
+
+    return{
+        loop:mainLoop.loop,
+        set_ups,
+    }
+}();
+
+export{MainLoop};
